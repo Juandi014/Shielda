18b87d8e9c1e6b32821ef92b61f0bda9b37d039d
nat-esp <natyespinosa2006@gmail.com>
2025-06-07 17:01:40 -0500

    User service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+  list(): Observable<User[]> {
+    return this.http.get<User[]>(`${environment.url_ms_cinema}/api/users`);
+  }
+  view(id: number): Observable<User> {
+    return this.http.get<User>(`${environment.url_ms_cinema}/api/users/${id}`);
+  }
+  create(newUser: User): Observable<User> {
+    delete newUser.id;
+    return this.http.post<User>(`${environment.url_ms_cinema}/api/users`, newUser);
+  }
+  update(theUser: User): Observable<User> {
+    return this.http.put<User>(`${environment.url_ms_cinema}/api/users/${theUser.id}`, theUser);
+  }
+
+  delete(id: number) {
+    return this.http.delete<User>(`${environment.url_ms_cinema}/api/users/${id}`);
+  }
+
+}
